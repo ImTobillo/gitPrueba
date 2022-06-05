a9c51a7f1fc4b542249678796dc15dc3fc2ade7c
Lautaro <lau_manzo12@hotmail.com>
2022-06-05 20:53:37 -0300

    Agregue texto en el main

--- src/gitPrueba/Main.java
+++ src/gitPrueba/Main.java
@@ -3,7 +3,10 @@ package gitPrueba;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("hola mundo");
+		
+		System.out.println("Parcial de lab 22/6");
+		
+		
 
 	}
 
